82ca3ad27b4f0de8c90bc8e1d57a99e7d3abab75
nolte <nolte07@googlemail.com>
2018-04-15 00:18:18 +0200

    add gateway

---
+++ spring-boot-example-gateway-service/src/main/java/eu/hermes/esb/example/gateway/GatewayServiceApplication.java
@@ -0,0 +1,19 @@
+package eu.hermes.esb.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableZuulProxy
+@EnableEurekaClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
