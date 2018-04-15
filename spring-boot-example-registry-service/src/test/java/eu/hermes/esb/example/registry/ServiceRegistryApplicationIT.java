fbbd06b3e51a18fedd8c206d1a0e8289f1044b18
nolte <nolte07@googlemail.com>
2018-04-15 12:09:56 +0200

    add tests

---
+++ spring-boot-example-registry-service/src/test/java/eu/hermes/esb/example/registry/ServiceRegistryApplicationIT.java
@@ -0,0 +1,31 @@
+package eu.hermes.esb.example.registry;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceRegistryApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles({ "test" })
+public class ServiceRegistryApplicationIT {
+
+	@Autowired
+	TestRestTemplate testRestTemplate;
+
+	@Test
+	public void loadSimpleConfigOverHttpWithProfile() {
+		ResponseEntity<Map> config = testRestTemplate.getForEntity("/info", Map.class);
+		assertEquals(HttpStatus.OK, config.getStatusCode());
+	}
+
+}
