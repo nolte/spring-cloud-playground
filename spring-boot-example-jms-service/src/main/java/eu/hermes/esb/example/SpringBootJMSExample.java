eb9d5b2ea2a19c07876c262a1fed626667b61343
nolte <nolte@ingo.de>
2018-04-26 22:46:47 +0200

    add camel zipkin

--- spring-boot-example-jms-service/src/main/java/eu/hermes/esb/example/SpringBootJMSExample.java
+++ spring-boot-example-jms-service/src/main/java/eu/hermes/esb/example/SpringBootJMSExample.java
@@ -1,5 +1,6 @@
 package eu.hermes.esb.example;
 
+import org.apache.camel.zipkin.starter.CamelZipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 @EnableAutoConfiguration
 @EnableJms
 @EnableTransactionManagement
+@CamelZipkin
 public class SpringBootJMSExample {
 
   public static void main(String[] args) throws Exception {
